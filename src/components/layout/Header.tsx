bd5c694561290f2d38f4e551276a3dbb26c878b7
Ruqayah1204 <mhiedey98@gmail.com>
2025-09-23 21:56:17 +0100

    trasnsaction history components added

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -53,7 +53,7 @@ const Header = ({onMenuToggle}: menuProps) => {
         "/user/transactions": { 
             title: "Transactions", 
             bg: "bg-megagreen text-white",
-            input: "",
+            input: "text-icon",
             hasSearch: true,
             searchPlaceholder: "Search for something..."
         },
@@ -67,8 +67,8 @@ const Header = ({onMenuToggle}: menuProps) => {
     };
 
     return (
-        <header className={`px-6 py-4 font-poppins ${defaultSetting.bg}`}>
-            <div className="flex items-center justify-between">
+        <header className={`px-4 lg:px-6 py-4 font-poppins ${defaultSetting.bg}`}>
+            <div className="flex items-center justify-between gap-4">
                 {/* Left side - Page Title */}
                 <div className="flex gap-3">
                     <Button
@@ -83,21 +83,21 @@ const Header = ({onMenuToggle}: menuProps) => {
                         {defaultSetting.title}
                     </h1>
                 </div>
-                <div className="flex items-center gap-10">
+                <div className="flex items-center gap-4 ">
                     {/* Center - Conditional Search */}
                     {defaultSetting.hasSearch && (
-                        <div className="relative">
+                        <div className="relative hidden sm:flex bg-white rounded-full">
                             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-icon w-4 h-4" />
                             <Input
                                 type="text"
                                 placeholder={defaultSetting.searchPlaceholder || "Search..."}
-                                className="pl-10 w-70 border-transparent shadow-none placeholder:text-icon"
+                                className={`pl-10 w-70 border-transparent shadow-none placeholder:text-icon ${defaultSetting.input}`}
                             />
                         </div>
                     )}
                     {/* Right side - User actions */}
                     <div className="flex items-center gap-3">
-                        <Button variant="ghost" size="icon" className="hover:bg-white/10">
+                        <Button variant="ghost" size="icon" className="hover:bg-white/10 hidden sm:block">
                             <Bell className="w-5 h-5" />
                         </Button>
                         
